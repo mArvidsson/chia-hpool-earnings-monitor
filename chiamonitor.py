4b8734a1695eb0d1ca0b8167887b5be9a0da6181
mArvidsson <arvidsson.marcus@hotmail.com>
2021-09-14 23:42:27 +0200

    More updates..

--- chiamonitor.py
+++ chiamonitor.py
@@ -1,3 +1,111 @@
-import chiamail
+# Email libraries
+import imaplib 
+import email
+import json
+from time import sleep
+from datetime import datetime
 
-print(str(chiamail.getEarnings(0)))
+mail = imaplib.IMAP4_SSL("imap.gmail.com") # Standard protocol, port defaults to 993
+id_list = [] # Initiate variable
+mail_init = 0
+
+########################################################################################
+###########                   BEGIN PARTS FOR MAIL HANDLING             ################
+########################################################################################            
+
+# # # # # # # # # # # # # # # #
+# Main mail import functions  #
+# # # # # # # # # # # # # # # #
+
+def init_mail():
+    # Initialise mail before trying to use the inbox
+    # This method will be automatically handled by getEarnings
+    global id_list
+    global mail_init
+    try:
+        mail.login("x@gmail.com", "x") # Gmail address and application password
+        mail.select("chia") # Select inbox, in my case label is "chia" rather than "INBOX"
+        id_list = get_email_list() # Get newest index of inbox
+        mail_init = 1
+    except:
+        print("Failed to authenticate. Exiting..")
+        quit()
+    
+def exit_mail():
+    # Exit mailbox when not in use.. this needs to be done manually
+    global id_list
+    global mail_init
+    # Make sure we don't keep an open connection once we're done with business
+    try:
+        mail.close()
+        mail.logout()
+        mail_init = 0
+        id_list = []
+    except:
+        print("Failed to close mailbox!")
+
+def get_email_list():
+    # Method to index the selected inbox
+    # which in turns allows us to select
+    # emails from ID 0 to the max index
+
+    try:
+        x, data = mail.search(None, "ALL") # Just read all mails in box.. why not? 
+        mail_ids = data[0]
+        list_ids = mail_ids.split()
+        list_ids.reverse() # Reverse list to put most recent emails first in index
+        return list_ids
+    except:
+        return("Failed to fetch email list. Exiting..")
+        quit()
+
+# # # # # # # # # # # # # # # # # # # 
+# Main mail data fetching functions #
+# # # # # # # # # # # # # # # # # # # 
+
+def getEarnings(email_index):
+    # Method to fetch hpool earnings in 
+    # the format 0.XXXXXXXX from chia inbox
+
+    if mail_init == 0: # Initiate mail if not already done
+        init_mail()
+
+    x, msg = mail.fetch(str(int(id_list[email_index])), "(RFC822)") # Fetch the whole damn email
+
+    for response_part in msg:
+        if isinstance(response_part, tuple):
+            try:
+                msg = email.message_from_bytes(response_part[1]).get_payload(decode=True).decode() # Decrypt text from bytes
+                chia_earnings = str(msg.split(":")[1]).split("C")[0] # Split earnings from message body
+                
+                return float(chia_earnings)# Try to return a float
+            except:
+                return []
+
+def getLifetimeEarnings():
+    # Now we just loop through our beautiful getEarnings method
+    # to calculate just how much we've received according to inbox
+
+    # but first we declare some stuff outside the loop..
+    prev_earnings = 0
+    earnings = 0
+    i = 0
+
+    for emails in id_list: # Count emails
+        new_earnings = getEarnings(i)           # - Only get earnings once for the loop for this email, then add them together
+        if not prev_earnings == new_earnings:   # - Make sure we don't read the same email twice (hPool somtimes sends x2)
+            earnings = earnings + new_earnings  # - Increase earnings
+            prev_earnings = new_earnings        # - Track new earnings as previous for the next instance
+        i+=1                                    # - Increment counter 
+
+    return earnings
+
+x = datetime.now()
+sleep(5)
+y = datetime.now()
+print(y.strftime("%A, %d. %B %Y %H:%M"))
+print(datetime.now() > x)
+print(type(x))
+print("Latest earnings:" + str(getEarnings(0)))
+#print("Getting lifetime earnings..")
+#print("Lifetime earnings: " + str(getLifetimeEarnings())[:10])
--- xch.py
+++ xch.py
@@ -7,12 +7,11 @@ import json
 import requests
 import adafruit_character_lcd.character_lcd as characterlcd
 
-# Modify this if you have a different sized character LCD
+# Initialise screen characters
 lcd_columns = 16
 lcd_rows = 2
 
-# compatible with all versions of RPI as of Jan. 2019
-# v1 - v3B+
+# Initialise RPi IO 
 lcd_rs = digitalio.DigitalInOut(board.D26)
 lcd_en = digitalio.DigitalInOut(board.D19)
 lcd_d4 = digitalio.DigitalInOut(board.D13)
@@ -21,78 +20,47 @@ lcd_d6 = digitalio.DigitalInOut(board.D5)
 lcd_d7 = digitalio.DigitalInOut(board.D11)
 lcd_backlight = digitalio.DigitalInOut(board.D9) # not connected
 
-# Initialise the lcd class
+# Initialise the LCD with class
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                                       lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-# custom char
-arrowup =   bytes([0b00000,
-                   0b00100,
-                   0b00100,
-                   0b01110,
-                   0b01110,
-                   0b11111,
-                   0b11111,
-                   0b00000])
+# Create custom characters for display
+lcd.create_char(0, [0x00, 0x04, 0x04, 0x0E, 0x0E, 0x1F, 0x04, 0x04]) # Arrow up
+lcd.create_char(1, [0x00, 0x04, 0x04, 0x1F, 0x0E, 0x0E, 0x04, 0x04]) # Arrow down
+lcd.create_char(2, [0x04, 0x04, 0x1F, 0x04, 0x04, 0x00, 0x1F, 0x00]) # +-
 
-arrowdown = bytes( [0b00000,
-                    0b11111,
-                    0b11111,
-                    0b01110,
-                    0b01110,
-                    0b00100,
-                    0b00100,
-                    0b00000])
-        
-plusminus = bytes( [0b00100,
-                    0b00100,
-                    0b11111,
-                    0b00100,
-                    0b00100,
-                    0b00000,
-                    0b11111,
-                    0b00000])
-                  
-# Store in LCD character memory 0
-lcd.create_char(0, arrowup)
-lcd.create_char(1, arrowdown)
-lcd.create_char(2, plusminus)
-
-# wipe LCD screen before we start
-lcd.clear()
-
-# start screen
-lcd.backlight = True
-
-# set last price empty
 lastPrice = 0
 
-# try fetching last price from file
-try:
-    with open("price_data.json", "r") as read_file:
-        price_data = json.load(read_file)
-        lastPrice = price_data["lastPrice"]
-        print("Fetched last price successfully! $" + str(lastPrice) + " at " + str(price_data["timeStamp"]))
-except:
-    print ("Error parsing price data")
+def getSavedPrice():
+    try:
+        with open("data.json", "r") as read_file:
+            price_data = json.load(read_file)["priceData"]
+            old_price = price_data["lastPrice"]
+            print("Fetched last price successfully! $" + str(old_price) + " at " + str(price_data["timeStamp"]))
+            return old_price
+    except:
+         print("Error parsing price data")
 
-# get XCH price
 def xchPrice():
     try:
         b = requests.get('https://min-api.cryptocompare.com/data/price?fsym=XCH&tsyms=USD')
-        priceFloat = float(json.loads(b.text)['USD'])
+        price = float(json.loads(b.text)['USD'])
         
-        # save last known price with time stamp before overwriting
-        price_data = { 
-            "lastPrice": lastPrice,
-            "timeStamp": datetime.now().strftime(' %b %d %H:%M:%S')
-            }
+        
+        # save last known price with time stamp before overwriting, if it's new data
+        if not lastPrice == price:
+            price_data = { 
+                          "priceData": {
+                            "lastPrice": lastPrice,
+                            "timeStamp": datetime.now().strftime(' %b %d %H:%M:%S')
+                            }
+                          }
 
-        with open("price_data.json", "w") as write_file:
-            json.dump(price_data, write_file)
-            print("Saved price data: $" + str(lastPrice) + " on " + datetime.now().strftime(' %b %d %H:%M:%S') +". New price: $" + str(priceFloat))
-
-        return priceFloat
+            with open("data.json", "w") as write_file:
+                json.dump(price_data, write_file)
+                print("Saved price data: $" + str(lastPrice) + " on " + datetime.now().strftime(' %b %d %H:%M:%S') +". New price: $" + str(price))
+                
+        return price
 
     except requests.ConnectionError:
         print ("Error querying Crytocompare API")
@@ -107,40 +75,41 @@ def arrow(price_input):
         elif lastPrice > currentPrice:
            return "\x01"
         elif lastPrice == currentPrice:
-            return  "\x02"
+            return "\x02"
             
     except:
         print("Error comparing prices.")
 
+# wipe LCD screen before we start
+lcd.clear()
+
+# start screen
+lcd.backlight = True
+
+# set last price empty
+lastPrice = getSavedPrice()
+
+# Main loop
 while True:
-    try:
-        # show xch price
-        price = xchPrice()
-        
-        # clear screen if price is less than 7 characters
-        # because otherwise we'll get some weird overlaps..
-        if len(str(price)) < 6:
-            lcd.clear()
-            print("Cleared screen to prevent decimal issues!")
-        
-        # print current price
-        lcd_line_1 = " XCH: $" + str(price) + str(arrow(price)) + "\n"
-        
-        # update to last known price after we've compared
-        lastPrice = price
-
-        # date and time
-        lcd_line_2 = datetime.now().strftime(' %b %d %H:%M:%S')
-
-        # combine both lines into one update to the display
-        lcd.message = lcd_line_1 + lcd_line_2
-
-        sleep(5)
-    except KeyboardInterrupt:
+    # show xch price
+    price = xchPrice()
+    
+    # clear screen if price is less than 7 characters
+    # because otherwise we'll get some weird overlaps..
+    if len(str(price)) < 6:
         lcd.clear()
-        lcd.backlight = False
-        sys.exit(0)
+        print("Cleared screen to prevent decimal issues!")
+    
+    # print current price
+    lcd_line_1 = " XCH: $" + str(price) + str(arrow(price)) + "\n"
+    
+    # update to last known price after we've compared
+    lastPrice = price
 
+    # date and time
+    lcd_line_2 = datetime.now().strftime(' %b %d %H:%M:%S')
 
-# Fetch time: datetime.now().strftime(' %b %d %H:%M:%S')
+    # combine both lines into one update to the display
+    lcd.message = lcd_line_1 + lcd_line_2
 
+    sleep(5)
